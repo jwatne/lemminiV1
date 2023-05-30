6d6128e986a56b2de3db52bc888f1c782a255efe
jwatne <john.watne@gmail.com>
2023-05-29 21:40:06 -0500

    GroupBitfield and Icons Checkstyle.

--- Game/Icons.java
+++ Game/Icons.java
@@ -34,198 +34,238 @@ import tools.ToolBox;
  *
  * @author Volker Oth
  */
-public class Icons {
+public final class Icons {
 
-	/** icon width in pixels. */
-	public final static int WIDTH = 32;
-	/** icon height in pixels. */
-	public final static int HEIGHT = 40;
+    /**
+     * Number of icon sprites.
+     */
+    private static final int NUM_ICON_SPRITES = 15;
+    /** icon width in pixels. */
+    public static final int WIDTH = 32;
+    /** icon height in pixels. */
+    public static final int HEIGHT = 40;
 
-	/** Icon types. */
-	public static enum Type {
-		/** minus icon. */
-		MINUS,
-		/** plus icon. */
-		PLUS,
-		/** climber icon. */
-		CLIMB,
-		/** floater icon. */
-		FLOAT,
-		/** bomber icon. */
-		BOMB,
-		/** blocker icon. */
-		BLOCK,
-		/** builder icon. */
-		BUILD,
-		/** basher icon. */
-		BASH,
-		/** miner icon. */
-		MINE,
-		/** digger icon. */
-		DIG,
-		/** pause icon. */
-		PAUSE,
-		/** nuke icon. */
-		NUKE,
-		/** fast forward icon. */
-		FFWD,
-		/** an empty icon (not used). */
-		EMPTY,
-		/** invalid no such icon. */
-		INVALID;
+    /** Icon types. */
+    public enum Type {
+        /** minus icon. */
+        MINUS,
+        /** plus icon. */
+        PLUS,
+        /** climber icon. */
+        CLIMB,
+        /** floater icon. */
+        FLOAT,
+        /** bomber icon. */
+        BOMB,
+        /** blocker icon. */
+        BLOCK,
+        /** builder icon. */
+        BUILD,
+        /** basher icon. */
+        BASH,
+        /** miner icon. */
+        MINE,
+        /** digger icon. */
+        DIG,
+        /** pause icon. */
+        PAUSE,
+        /** nuke icon. */
+        NUKE,
+        /** fast forward icon. */
+        FFWD,
+        /** an empty icon (not used). */
+        EMPTY,
+        /** invalid no such icon. */
+        INVALID;
 
-		private static final Map<Integer,Type> lookup = new HashMap<Integer,Type>();
+        /**
+         * Map for icon lookup.
+         */
+        private static final Map<Integer, Type> LOOKUP = new HashMap<>();
 
-		static {
-			for(Type s : EnumSet.allOf(Type.class))
-				lookup.put(s.ordinal(), s);
-		}
+        static {
+            for (Type s : EnumSet.allOf(Type.class)) {
+                LOOKUP.put(s.ordinal(), s);
+            }
+        }
 
-		/**
-		 * Reverse lookup implemented via hashtable.
-		 * @param val Ordinal value
-		 * @return Parameter with ordinal value val
-		 */
-		public static Type get(final int val) {
-			return lookup.get(val);
-		}
-	}
+        /**
+         * Reverse lookup implemented via hashtable.
+         *
+         * @param val Ordinal value
+         * @return Parameter with ordinal value val
+         */
+        public static Type get(final int val) {
+            return LOOKUP.get(val);
+        }
+    }
 
-	/** 1st radio button. */
-	private final static int FIRST_RADIO = Type.CLIMB.ordinal();
-	/** last radio button. */
-	private final static int LAST_RADIO = Type.DIG.ordinal();
-	/** last icon to be drawn. */
-	private final static int LAST_DRAWN = Type.FFWD.ordinal();
+    /** 1st radio button. */
+    private static final int FIRST_RADIO = Type.CLIMB.ordinal();
+    /** last radio button. */
+    private static final int LAST_RADIO = Type.DIG.ordinal();
+    /** last icon to be drawn. */
+    private static final int LAST_DRAWN = Type.FFWD.ordinal();
 
-	/** array of Sprites that contains the icons. */
-	private static Sprite icons[];
-	/** buffered image that contains the whole icon bar in its current state. */
-	private static BufferedImage iconImg;
-	/** graphics object used to draw on iconImg. */
-	private static Graphics2D iconGfx;
+    /** array of Sprites that contains the icons. */
+    private static Sprite[] icons;
+    /** buffered image that contains the whole icon bar in its current state. */
+    private static BufferedImage iconImg;
+    /** graphics object used to draw on iconImg. */
+    private static Graphics2D iconGfx;
 
+    /**
+     * Private default constructor for utility class.
+     */
+    private Icons() {
 
-	/**
-	 * Initialization.
-	 * @param cmp parent component
-	 * @throws ResourceException
-	 */
-	public static void init(final Component cmp) throws ResourceException {
-		iconImg = ToolBox.createImage(WIDTH*(1+LAST_DRAWN),HEIGHT,Transparency.OPAQUE);
-		iconGfx = iconImg.createGraphics();
-		MediaTracker tracker = new MediaTracker(cmp);
-		icons = new Sprite[15];
-		for (int i=0; i<14; i++) {
-			Image sourceImg = Core.loadImage(tracker, "misc/icon_"+i+".gif");
-			try {
-				tracker.waitForAll();
-			} catch (InterruptedException ex) {}
-			icons[i] = new Sprite(sourceImg, (i==Type.EMPTY.ordinal())?1:2);
-			if (i<=LAST_DRAWN)
-				iconGfx.drawImage(icons[i].getImage(),WIDTH*i,0,null);
-		}
-	}
+    }
 
-	/**
-	 * Get Icon type by x position.
-	 * @param x x position inside bar in pixels
-	 * @return Icon type
-	 */
-	public static Type getType(final int x) {
-		if (x>=(LAST_DRAWN+1)*WIDTH)
-			return Type.INVALID; // invalid
-		return Type.get(x/WIDTH);
-	}
+    /**
+     * Initialization.
+     *
+     * @param cmp parent component
+     * @throws ResourceException
+     */
+    public static void init(final Component cmp) throws ResourceException {
+        iconImg = ToolBox.createImage(WIDTH * (1 + LAST_DRAWN), HEIGHT,
+                Transparency.OPAQUE);
+        iconGfx = iconImg.createGraphics();
+        MediaTracker tracker = new MediaTracker(cmp);
+        icons = new Sprite[NUM_ICON_SPRITES];
 
-	/**
-	 * Get buffered image that contains the whole icon bar in its current state.
-	 * @return image of icon bar
-	 */
-	public static BufferedImage getImg() {
-		return iconImg;
-	}
+        for (int i = 0; i < (NUM_ICON_SPRITES - 1); i++) {
+            Image sourceImg = Core.loadImage(tracker,
+                    "misc/icon_" + i + ".gif");
 
-	/**
-	 * Get pressed state of the given Icon
-	 * @param type
-	 * @return
-	 */
-	static boolean isPressed(Type type) {
-		int idx = type.ordinal();
-		if (idx > LAST_DRAWN)
-			return false;
-		return (icons[idx].getFrameIdx() == 1);
-	}
+            try {
+                tracker.waitForAll();
+            } catch (InterruptedException ex) {
+            }
 
-	/**
-	 * Press down icon.
-	 * @param type Icon Type
-	 */
-	static void press(final Type type) {
-		int idx = type.ordinal();
+            icons[i] = new Sprite(sourceImg,
+                    (i == Type.EMPTY.ordinal()) ? 1 : 2);
 
-		switch (type) {
-			case PAUSE:
-			case FFWD:
-				icons[idx].setFrameIdx((icons[idx].getFrameIdx()==0)?1:0); // toggle
-				if (idx <= LAST_DRAWN)
-					iconGfx.drawImage(icons[idx].getImage(),WIDTH*idx,0,null);
-				break;
-			case CLIMB:
-			case FLOAT:
-			case BOMB:
-			case BLOCK:
-			case BUILD:
-			case BASH:
-			case MINE:
-			case DIG:
-				for (int i=FIRST_RADIO; i<=LAST_RADIO; i++)
-					if (i!=idx) {
-						icons[i].setFrameIdx(0);
-						iconGfx.drawImage(icons[i].getImage(),WIDTH*i,0,null);
-					}
-				//$FALL-THROUGH$
-			case MINUS:
-			case PLUS:
-			case NUKE:
-				icons[idx].setFrameIdx(1); // set "pressed" frame
-				if (idx <= LAST_DRAWN)
-					iconGfx.drawImage(icons[idx].getImage(),WIDTH*idx,0,null);
-				break;
+            if (i <= LAST_DRAWN) {
+                iconGfx.drawImage(icons[i].getImage(), WIDTH * i, 0, null);
+            }
+        }
+    }
 
+    /**
+     * Get Icon type by x position.
+     *
+     * @param x x position inside bar in pixels
+     * @return Icon type
+     */
+    public static Type getType(final int x) {
+        if (x >= (LAST_DRAWN + 1) * WIDTH) {
+            return Type.INVALID; // invalid
+        }
+        return Type.get(x / WIDTH);
+    }
 
-			default:
-				break;
-		}
-	}
+    /**
+     * Get buffered image that contains the whole icon bar in its current state.
+     *
+     * @return image of icon bar
+     */
+    public static BufferedImage getImg() {
+        return iconImg;
+    }
 
-	/**
-	 * Release Icon.
-	 * @param type Icon Type
-	 */
-	static void release(final Type type) {
-		int idx = type.ordinal();
-		switch (type) {
-			case MINUS:
-			case PLUS:
-			case NUKE:
-				icons[idx].setFrameIdx(0); // set "released" frame
-				if (idx <= LAST_DRAWN)
-					iconGfx.drawImage(icons[idx].getImage(),WIDTH*idx,0,null);
-				break;
-			default:
-				break;
-		}
-	}
+    /**
+     * Get pressed state of the given Icon.
+     *
+     * @param type
+     * @return pressed state of the Icon.
+     */
+    static boolean isPressed(final Type type) {
+        int idx = type.ordinal();
 
-	/**
-	 * Reset Icon bar.
-	 */
-	static void reset() {
-		for (int i=0; i<=LAST_DRAWN; i++) {
-			icons[i].setFrameIdx(0);
-			iconGfx.drawImage(icons[i].getImage(),WIDTH*i,0,null);
-		}
-	}
+        if (idx > LAST_DRAWN) {
+            return false;
+        }
+
+        return (icons[idx].getFrameIdx() == 1);
+    }
+
+    /**
+     * Press down icon.
+     *
+     * @param type Icon Type
+     */
+    static void press(final Type type) {
+        int idx = type.ordinal();
+
+        switch (type) {
+        case PAUSE:
+        case FFWD:
+            icons[idx].setFrameIdx((icons[idx].getFrameIdx() == 0) ? 1 : 0);
+            // toggle
+
+            if (idx <= LAST_DRAWN) {
+                iconGfx.drawImage(icons[idx].getImage(), WIDTH * idx, 0, null);
+            }
+
+            break;
+        case CLIMB:
+        case FLOAT:
+        case BOMB:
+        case BLOCK:
+        case BUILD:
+        case BASH:
+        case MINE:
+        case DIG:
+            for (int i = FIRST_RADIO; i <= LAST_RADIO; i++) {
+                if (i != idx) {
+                    icons[i].setFrameIdx(0);
+                    iconGfx.drawImage(icons[i].getImage(), WIDTH * i, 0, null);
+                }
+            }
+            //$FALL-THROUGH$
+        case MINUS:
+        case PLUS:
+        case NUKE:
+            icons[idx].setFrameIdx(1); // set "pressed" frame
+            if (idx <= LAST_DRAWN) {
+                iconGfx.drawImage(icons[idx].getImage(), WIDTH * idx, 0, null);
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    /**
+     * Release Icon.
+     *
+     * @param type Icon Type
+     */
+    static void release(final Type type) {
+        int idx = type.ordinal();
+        switch (type) {
+        case MINUS:
+        case PLUS:
+        case NUKE:
+            icons[idx].setFrameIdx(0); // set "released" frame
+            if (idx <= LAST_DRAWN) {
+                iconGfx.drawImage(icons[idx].getImage(), WIDTH * idx, 0, null);
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+    /**
+     * Reset Icon bar.
+     */
+    static void reset() {
+        for (int i = 0; i <= LAST_DRAWN; i++) {
+            icons[i].setFrameIdx(0);
+            iconGfx.drawImage(icons[i].getImage(), WIDTH * i, 0, null);
+        }
+    }
 }

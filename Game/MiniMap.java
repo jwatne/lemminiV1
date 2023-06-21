ce64719c1d61d7be0d3e8383236dd8c48d26dbd6
jwatne <john.watne@gmail.com>
2023-06-21 02:28:41 -0500

    MiniMap checkstyle.

--- Game/MiniMap.java
+++ Game/MiniMap.java
@@ -22,100 +22,123 @@ import java.awt.image.BufferedImage;
 
 /**
  * Handles the mini map.
+ *
  * @author Volker Oth
  */
-public class MiniMap {
+public final class MiniMap {
 
-	/** color of Lemmings in mini map. */
-	final private static Color LEMM_COLOR = Color.RED;
-	/** color of screen frame in mini map. */
-	final private static Color BORDER_COLOR = Color.YELLOW;
+    /** color of Lemmings in mini map. */
+    private static final Color LEMM_COLOR = Color.RED;
+    /** color of screen frame in mini map. */
+    private static final Color BORDER_COLOR = Color.YELLOW;
 
-	/** image used for mini map. */
-	private static BufferedImage img;
-	/** X position in main gfx. */
-	private static int xPos;
-	/** Y position in main gfx. */
-	private static int yPos;
-	/** X scale. */
-	private static int scaleX;
-	/** Y scale. */
-	private static int scaleY;
-	/** height of mini map. */
-	private static int height;
-	/** width of mini map. */
-	private static int width;
+    /** image used for mini map. */
+    private static BufferedImage img;
+    /** X position in main gfx. */
+    private static int xPos;
+    /** Y position in main gfx. */
+    private static int yPos;
+    /** X scale. */
+    private static int scaleX;
+    /** Y scale. */
+    private static int scaleY;
+    /** height of mini map. */
+    private static int height;
+    /** width of mini map. */
+    private static int width;
 
-	/**
-	 * init
-	 * @param x X position in main gfx used in drawLemming() and move()
-	 * @param y Y position in main gfx used in drawLemming() and move()
-	 * @param sx X Scale (2 -> 0.5)
-	 * @param sy Y Scale (3 -> 0.333)
-	 * @param tint true: apply greenish tint, false: use original colors
-	 */
-	public static void init(final int x, final int y, final int sx, final int sy, final boolean tint) {
-		xPos = x;
-		yPos = y;
-		scaleX = sx;
-		scaleY = sy;
-		Level level = GameController.getLevel();
-		BufferedImage bgImage = GameController.getBgImage();
-		img = level.createMiniMap(img, bgImage, scaleX, scaleY, tint);
-		width = img.getWidth();
-		height = img.getHeight();
-	}
+    /**
+     * Private default constructor for utility class.
+     */
+    private MiniMap() {
 
-	/**
-	 * Draw mini map.
-	 * @param g Graphics object to draw on
-	 * @param x x position in pixels
-	 * @param y y position in pixels
-	 * @param xOfs horizontal level offset
-	 */
-	public static void draw(final Graphics2D g, final int x, final int y, final int xOfs) {
-		int wWidth = Core.getDrawWidth();
-		g.drawImage(img,x,y,null);
-		g.setColor(BORDER_COLOR);
-		g.drawRect(x+xOfs/scaleX,y,wWidth/scaleX,img.getHeight()-1);
-	}
+    }
 
-	/**
-	 * Draw Lemming in mini map.
-	 * @param g Graphics object to draw on
-	 * @param lx original lemming x position in pixels
-	 * @param ly original lemming y position in pixels
-	 */
-	public static void drawLemming(final Graphics2D g, final int lx, final int ly) {
-		int x = xPos + (lx+scaleX/2)/scaleX;
-		int y = yPos + (ly+scaleY/2)/scaleY;
-		g.setColor(LEMM_COLOR);
-		g.fillRect(x,y,2,2);
-	}
+    /**
+     * Initialize.
+     *
+     * @param x    X position in main gfx used in drawLemming() and move()
+     * @param y    Y position in main gfx used in drawLemming() and move()
+     * @param sx   X Scale (2 -> 0.5)
+     * @param sy   Y Scale (3 -> 0.333)
+     * @param tint true: apply greenish tint, false: use original colors
+     */
+    public static void init(final int x, final int y, final int sx,
+            final int sy, final boolean tint) {
+        xPos = x;
+        yPos = y;
+        scaleX = sx;
+        scaleY = sy;
+        Level level = GameController.getLevel();
+        BufferedImage bgImage = GameController.getBgImage();
+        img = level.createMiniMap(img, bgImage, scaleX, scaleY, tint);
+        width = img.getWidth();
+        height = img.getHeight();
+    }
 
-	/**
-	 * Return current image.
-	 * @return current image.
-	 */
-	public static BufferedImage getImage() {
-		return img;
-	}
+    /**
+     * Draw mini map.
+     *
+     * @param g    Graphics object to draw on
+     * @param x    x position in pixels
+     * @param y    y position in pixels
+     * @param xOfs horizontal level offset
+     */
+    public static void draw(final Graphics2D g, final int x, final int y,
+            final int xOfs) {
+        int wWidth = Core.getDrawWidth();
+        g.drawImage(img, x, y, null);
+        g.setColor(BORDER_COLOR);
+        g.drawRect(x + xOfs / scaleX, y, wWidth / scaleX, img.getHeight() - 1);
+    }
 
-	/**
-	 * Move screen frame via mini map.
-	 * @param x cursor x position in original gfx.
-	 * @param y cursor y position in original gfx.
-	 * @param swidth screen width
-	 * @return new horizontal screen offset
-	 */
-	public static int move(final int x, final int y, final int swidth) {
-		if (y < yPos || y >= yPos+height || x < xPos || x >= xPos+width)
-			return -1; // cursor outside the mini map
-		int xOfs = (x-xPos)*scaleX-swidth/2;
-		if (xOfs < 0)
-			xOfs = 0;
-		if (xOfs > Level.WIDTH - swidth)
-			xOfs = Level.WIDTH - swidth-1;
-		return xOfs;
-	}
+    /**
+     * Draw Lemming in mini map.
+     *
+     * @param g  Graphics object to draw on
+     * @param lx original lemming x position in pixels
+     * @param ly original lemming y position in pixels
+     */
+    public static void drawLemming(final Graphics2D g, final int lx,
+            final int ly) {
+        int x = xPos + (lx + scaleX / 2) / scaleX;
+        int y = yPos + (ly + scaleY / 2) / scaleY;
+        g.setColor(LEMM_COLOR);
+        g.fillRect(x, y, 2, 2);
+    }
+
+    /**
+     * Return current image.
+     *
+     * @return current image.
+     */
+    public static BufferedImage getImage() {
+        return img;
+    }
+
+    /**
+     * Move screen frame via mini map.
+     *
+     * @param x      cursor x position in original gfx.
+     * @param y      cursor y position in original gfx.
+     * @param swidth screen width
+     * @return new horizontal screen offset
+     */
+    public static int move(final int x, final int y, final int swidth) {
+        if (y < yPos || y >= yPos + height || x < xPos || x >= xPos + width) {
+            return -1; // cursor outside the mini map
+        }
+
+        int xOfs = (x - xPos) * scaleX - swidth / 2;
+
+        if (xOfs < 0) {
+            xOfs = 0;
+        }
+
+        if (xOfs > Level.WIDTH - swidth) {
+            xOfs = Level.WIDTH - swidth - 1;
+        }
+
+        return xOfs;
+    }
 }
